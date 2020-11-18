f890d6df0b907c94c047e4914f487d37c3a06708
Nikhil Sujith <nik98hil@gmail.com>
2020-11-18 17:14:32 -0500

    side nav changed

--- Forms/Code/html/index.php
+++ Forms/Code/html/index.php
@@ -1,10 +1,8 @@
 <?php
-    include("Forms/Code/php/connection.php")
-    echo "hello"
+    include("../php/connection.php");
 ?>
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
@@ -18,25 +16,7 @@
 
 <body id="page-top">
     <div id="wrapper">
-        <nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0">
-            <div class="container-fluid d-flex flex-column p-0">
-                <a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
-                    <div class="sidebar-brand-icon rotate-n-15"></div>
-                    <div class="sidebar-brand-text mx-3"></div>
-                </a>
-                <hr class="sidebar-divider my-0">
-                <ul class="nav navbar-nav text-light" id="accordionSidebar">
-                    <li class="nav-item"><a class="nav-link active" href="index.html"><i class="fa fa-user-plus"></i><span>Create New User</span></a></li>
-                    <li class="nav-item"><a class="nav-link" href="profile.html"><i class="fas fa-user"></i>Create New Role</a></li>
-                    <li class="nav-item"><a class="nav-link" href="table.html"><i class="fas fa-table"></i>Create New&nbsp;<span>Table</span></a></li>
-                    <li class="nav-item"><a class="nav-link" href="login.html"><i class="far fa-user-circle"></i>Insert New Privilege</a></li>
-                    <li class="nav-item"><a class="nav-link" href="register.html"><i class="fas fa-user-circle"></i>Relate User Account to Role</a></li>
-                    <li class="nav-item"><a class="nav-link" href="blank.html"><i class="fas fa-window-maximize"></i><span>Relate Acc Priv to Role</span></a><a class="nav-link" href="blank.html"><i class="fas fa-window-maximize"></i><span>Relate Relation Priv to Role and Table</span></a>
-                        <a
-                            class="nav-link" href="blank.html"><i class="fas fa-window-maximize"></i>Retrieve Role Privileges</a><a class="nav-link" href="blank.html"><i class="fas fa-window-maximize"></i>&nbsp;Retrieve User Privileges</a><a class="nav-link" href="blank.html"><i class="fas fa-window-maximize"></i><span>Check Privilege for User</span></a></li>
-                </ul>
-            </div>
-        </nav>
+        <?php  include("side-nav.php"); ?>
         <div class="d-flex flex-column" id="content-wrapper">
             <div id="content">
                 <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
