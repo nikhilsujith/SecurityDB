46d76b76955e2ce1ad98dc9f41838d2cf560079f
Abhishek-Shinde-UTA <abhishek.shinde@mavs.uta.edu>
2020-11-19 09:20:05 -0600

    Added New Role

---
+++ Forms/Code/php/new-role-mysql.php
@@ -0,0 +1,31 @@
+<?php
+    $rolename = $_POST['rolename'];
+    $description = $_POST['description'];
+
+
+    include("connection.php");
+
+$sql = "INSERT INTO `user_role` (`roleName`, `description`)
+ VALUES ('$rolename','$description')";
+
+$checking = mysqli_query($conn, $sql);
+$url = "new_role.php";
+if ($checking) 
+        {   
+        // echo "query success";
+        echo '<script type="text/javascript">';
+        echo 'window.location.href="'.$url.'";';
+        echo '</script>';
+        echo '<noscript>';
+        echo '<meta http-equiv="refresh" content="0;url='.$url.'" />';
+        echo '</noscript>'; 
+            exit();
+        } 
+        else 
+        {   
+            echo "Query Error";
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        mysqli_close($conn);
+
+?>
