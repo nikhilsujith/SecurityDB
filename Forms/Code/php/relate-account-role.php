85d70264723356990b8199a16a036557d3c11127
Abhishek-Shinde-UTA <abhishek.shinde@mavs.uta.edu>
2020-11-20 13:58:28 -0600

    update before mymeeting

--- Forms/Code/php/relate-account-role.php
+++ Forms/Code/php/relate-account-role.php
@@ -4,11 +4,20 @@
 
     include("connection.php");
 //    check if the user ID being input already has a role
+
     $query_check_user_role = "SELECT userID FROM user_role WHERE userID = $userID";
     $result_check_user_role = mysqli_query($conn, $query_check_user_role);
 
     if(mysqli_num_rows($result_check_user_role) == 0){ //userID entered is not assigned a role
         $flag = 1;
+
+    $check_role_exists = "SELECT roleName FROM user_role WHERE roleName = '$roleName'";
+    $result_check_role_exists = mysqli_query($conn, $check_role_exists);//Checking if entered role exists in the role table
+
+
+
+
+
 //        Get the description for the role
         $query_get_role_desc = "SELECT description FROM user_role WHERE roleName = '$roleName'";
         $result_get_role_desc = mysqli_query($conn, $query_get_role_desc );
